f09cbcba44eef26ff7e0848c997ed8cb8f2f3729
vanard <vianixa34@gmail.com>
2025-03-14 10:02:10 +0700

    create expandable text widget

---
+++ food_restaurant_app/lib/widgets/expandable_text.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:food_restaurant_app/colors.dart';
+import 'package:food_restaurant_app/util/dimensions.dart';
+import 'package:food_restaurant_app/widgets/small_text.dart';
+
+class ExpandableTextWidget extends StatefulWidget {
+  final String text;
+
+  const ExpandableTextWidget({super.key, required this.text});
+
+  @override
+  State<ExpandableTextWidget> createState() => _ExpandableTextWidgetState();
+}
+
+class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
+  late String firstHalf;
+  late String secondHalf;
+  bool hiddenText = true;
+  double textHeight = Dimensions.screenHeight / 2;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.text.length > textHeight.toInt()) {
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf =
+          widget.text.substring(textHeight.toInt() + 1, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = '';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: secondHalf.isEmpty
+          ? SmallText(
+              text: firstHalf,
+              color: AppColors.paraColor,
+              size: Dimensions.font14,
+              height: 1.5,
+            )
+          : Column(
+              children: [
+                SmallText(
+                  text:
+                      hiddenText ? ("$firstHalf...") : (firstHalf + secondHalf),
+                  color: AppColors.paraColor,
+                  size: Dimensions.font14,
+                  height: 1.5,
+                ),
+                SizedBox(
+                  height: Dimensions.dimen5,
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      hiddenText = !hiddenText;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      SmallText(
+                        text: hiddenText ? "show more" : "show less",
+                        color: AppColors.mainColor,
+                      ),
+                      Icon(
+                        hiddenText
+                            ? Icons.arrow_drop_down
+                            : Icons.arrow_drop_up,
+                        color: AppColors.mainColor,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+    );
+  }
+}
+
+//Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
