de22de145a507de3d02c8924a6eb23a15ff21c0e
vanard <vianixa34@gmail.com>
2025-03-14 10:03:39 +0700

    accept iconSize now can modify iconSize AppIconWidget

--- food_restaurant_app/lib/widgets/app_icon.dart
+++ food_restaurant_app/lib/widgets/app_icon.dart
@@ -6,6 +6,7 @@ class AppIcon extends StatelessWidget {
   final Color backgroundColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
   final Function? function;
 
   const AppIcon(
@@ -14,7 +15,8 @@ class AppIcon extends StatelessWidget {
       required this.icon,
       this.backgroundColor = const Color(0xFFfcf4e4),
       this.iconColor = const Color(0xFF756d54),
-      this.size = 40});
+      this.size = 40,
+      this.iconSize = 16});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: Dimensions.font16, //size / 2.5,
+        size: iconSize == 16 ? Dimensions.font16 : iconSize, //size / 2.5,
       ),
     );
   }
